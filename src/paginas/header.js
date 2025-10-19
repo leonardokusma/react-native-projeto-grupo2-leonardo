ba5a9a1a05f44c6554cfeaa7b77535601311252d
Leonardo Kusma <leonardokusma48@gmail.com>
2025-10-19 12:15:51 -0300

    ADD component Header

---
+++ src/paginas/header.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {View, Text,StyleSheet, ImageBackground,Image} from 'react-native';
+import {useFonts,Poppins_400Regular} from '@expo-google-fonts/poppins'
+import imageBack from  '../assets/Vector.png';
+import hamburger from '../assets/Ícones/PNG/Hamburguer.png';
+
+
+
+export default function Header({title,user}){
+  const fontLoaded = useFonts({
+  Poppins_400Regular
+  });
+
+  if(!fontLoaded){
+    return null
+  }
+    return(
+        <ImageBackground source={imageBack}
+        style={styles.background}
+        resizeMode='cover'>
+        <View style={styles.header}>
+          <Image source={hamburger} style={styles.iconeHambur}/>
+          <Image source={user} style={styles.iconeUser}/>
+        </View>
+        <Text style={styles.headerText}>{title}</Text>
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    display:'flex',
+    flexDirection:'row',
+    alignItems:'center',
+    paddingTop:'14 %',
+    justifyContent:'space-between'
+  },
+  background:{
+    width:'100%',
+    height:'65%'
+  },
+  headerText:{
+    paddingTop:'20%',
+    fontSize: 20,
+    fontFamily:'Poppins_400Regular',
+    color:'#3772ff',
+    textAlign:'center'
+  },
+  iconeHambur:{
+    width:24,
+    height:24,
+    resizeMode:'contain',
+    paddingLeft:'8%',
+    paddingRight:'8%'
+  },
+  iconeUser:{
+    width:40,
+    height:40,
+    resizeMode:'contain',
+    paddingLeft:'8%',
+    paddingRight:'8%'
+  }
+})
